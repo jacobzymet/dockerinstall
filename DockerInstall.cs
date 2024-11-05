da70a2f707d72e1c45e2b80417c748946fad1d93
xiaoyong <54741819+xiaoyong-wang@users.noreply.github.com>
2024-11-04 22:53:31 -0500

    updates

---
+++ DockerInstall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DockerInstall
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Docker installation program.");
+            Console.WriteLine("We detected you are running " + new OSdetect().OS());
+        }
+    }
+    class OSdetect
+    {
+        public string OS()
+        {
+            string os = Environment.OSVersion.ToString();
+            return os;
+        }
+    }
+}
